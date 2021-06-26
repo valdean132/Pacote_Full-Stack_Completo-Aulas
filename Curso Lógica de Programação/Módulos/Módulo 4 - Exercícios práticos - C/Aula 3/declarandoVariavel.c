0fbb713f4135305ab8e22ea6bd60a3f3c630172d
valdean132 <65917804+valdean132@users.noreply.github.com>
2021-06-26 00:59:31 -0400

    Exercício prático #2 - C
    Peguei o exemplo do segundo modulo de algoritmo sobre calcular idade e executei no programa

---
+++ Curso Lógica de Programação/Módulos/Módulo 4 - Exercícios práticos - C/Aula 3/declarandoVariavel.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	
+	int ano_nascimento = 2000;
+	int ano_atual = 2021;
+	
+	int result = (ano_atual - ano_nascimento);
+	
+	printf("Sua Idade eh: %d anos", result);
+		
+	return 0;
+}
