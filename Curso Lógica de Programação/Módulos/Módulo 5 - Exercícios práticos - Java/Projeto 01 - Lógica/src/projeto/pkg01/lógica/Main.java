0917274bfb469e91ae1d346bf77fd09465baadba
valdean132 <65917804+valdean132@users.noreply.github.com>
2021-10-12 18:18:22 -0400

    Orientação a Objetos - Completo
    Nessa aula vi um pouco de orientação a objeto em Java, algumas classe e como instancia-las

---
+++ Curso Lógica de Programação/Módulos/Módulo 5 - Exercícios práticos - Java/Projeto 01 - Lógica/src/projeto/pkg01/lógica/Main.java
@@ -0,0 +1,14 @@
+
+package projeto.pkg01.lógica;
+
+
+public class Main {
+    
+    public static void main(String[] args){
+        Utils h = new Utils();
+        Utils.numero = 20;
+        System.out.println(h.numero);
+        
+    }
+    
+}
---
+++ Curso Lógica de Programação/Módulos/Módulo 5 - Exercícios práticos - Java/Projeto 01 - Lógica/src/projeto/pkg01/lógica/Utils.java
@@ -0,0 +1,12 @@
+
+package projeto.pkg01.lógica;
+
+
+public class Utils {
+    
+    public static int numero = 10;
+    
+    public void printHelloWord(){
+        System.out.println("Hello Word!");
+    }
+}
