cca9fd905eb0ba141793cd1c4999bf85a4f6758c
valdean132 <65917804+valdean132@users.noreply.github.com>
2022-08-12 05:54:42 -0400

    Python - Fundamentos
    Nessa aula vimos os fundamentos da linguagem Python, vimos como funciona um algoritmo de certa forma e escrevemos também nosso primeiro "Olá mundo" em Python.

---
+++ Curso de Python Completo/Módulos/Módulo #3 -  Variáveis e Tipos de Dados/Aula 01/aula01.py
@@ -0,0 +1,68 @@
+# Aula 01 - Fundamentos
+"""
+Algoritmo => É uma receita que mostra o passo a passo os procedimentos necessario para resolução de uma tarefa.
+Exemplo: Digamos que agora eu qeuira imprimir algo em uma impresspra, então vamos aos passos
+          Passo 01: Localize a impressora
+          Passo 02: Verificar se ela está ligada
+          Passo 03: Conectar a impressora ao dispositivo de onde irá sair a impressão
+        Agora no dispositivo
+          Passo 01: Localizar e abrir o arquivo a ser impresso
+          Passo 02: Solicitar a impressão
+
+Agora como vimos, em poucos passos poderemos fazer a impressão. Agora para um computador, quanto mais detalhista for, melhor será a execultado o programa com menos chaces de erros.
+
+  Agora vamos ao nosso primeiro comando em Python
+      O comando que veremos agora é o "print" que faz referência imprimir algo, nesse caso mostrar na tela.
+"""
+
+print("Olá mundo!") 
+
+"""
+
+    Agora como vimos, o python é uma linuagem bem simples em diversos aspequitos, uma dela é que não se pode utilizar o famoso ";", pois ele desconsidera e se usarmos ele, o mesmo nos gera um erro no cógido.
+
+"""
+
+""" 
+    Sobre o The Zen of Python, por Tim Peters
+
+        Bonito é melhor que feio.
+
+        Explícito é melhor que implícito.
+
+        Simples é melhor que complexo.
+
+        Complexo é melhor que complicado.
+
+        Plano é melhor que aglomerado.
+
+        Esparso é melhor que denso.
+
+        Legibilidade faz diferença.
+
+        Casos especiais não são especiais o bastante para quebrar as regras.
+
+        Embora a praticidade vença a pureza.
+
+        Erros nunca devem passar silenciosamente.
+
+        A menos que sejam explicitamente silenciados.
+
+        Diante da ambiguidade, recuse a tentação de adivinhar.
+
+        Deve haver um -- e preferencialmente só um -- modo óbvio para fazer algo.
+
+        Embora esse modo possa não ser óbvio à primeira vista a menos que você seja holandês.
+
+        Agora é melhor que nunca.
+
+        Embora nunca frequentemente seja melhor que *exatamente* agora.
+
+        Se a implementação é difícil de explicar, é uma má idéia.
+
+        Se a implementação é fácil de explicar, pode ser uma boa idéia.
+
+        Namespaces são uma grande idéia -- vamos fazer mais dessas!
+
+    Ele retrata sobre como o código deve ser bem oganizado, já que a compunidade python preza muito por essa organização do código, ou seja, a identação se faz necesario para que o código funcione de forma correta e legivel.
+"""
