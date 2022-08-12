286e69e9b9b7ad99ee24f8a42c043149863febb0
valdean132 <65917804+valdean132@users.noreply.github.com>
2022-08-12 07:22:36 -0400

    Python - Números
    Nessa aula um pouco sobre os números em Python e suas representatividades.

---
+++ Curso de Python Completo/Módulos/Módulo #3 -  Variáveis e Tipos de Dados/Aula 02/numeros.py
@@ -0,0 +1,17 @@
+# Aula 02 - Números
+""" 
+
+    Nessa Aula, será estudado os conjuntos dos números em Python.
+    No Python, temos 3 tipos de numeros imbutidos, snde eles: int, float e complex
+    
+    O int é o conjunte de numeros inteiros representados tanto pelos números negativos quanto os positivos que não possuem casas decimais.
+        Ex.: ... -3, -2, -1, 0, 1, 2, 3 ...
+
+    Os floats, são a representação de pontos flutuantes, ou seja, todo número que possui números decimais e que também não possuem os números deciamis, sendo eles positivos e negativos.
+        Ex.: ... -3, -3.4, -2, -2.5, -1, -1.5, 0, 1, 1.5, 2, 2.5, 3, 3.5 ...
+    Ele pode ser representado como 1.0, ou 1., ou até mesmo, 0.5 ou .5
+    
+    Para finalizar temos os complex que são os conjuntos de números complexos, são muito utilçizado em dataScient, estáticas, matemática, engenharia e muito mais. e são representados como os números complexos.
+        Ex.: 1+2j <== isso é um complex
+
+"""
