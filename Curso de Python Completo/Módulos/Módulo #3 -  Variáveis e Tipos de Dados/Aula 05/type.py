06b92625c493a6c03628c232865472d11dca6fd3
valdean132 <65917804+valdean132@users.noreply.github.com>
2022-08-15 06:56:21 -0400

    Python - Type
    Nessa aula vimos como utilizar a função Type para saber que tipo de dados eu estou utilizando.

---
+++ Curso de Python Completo/Módulos/Módulo #3 -  Variáveis e Tipos de Dados/Aula 05/type.py
@@ -0,0 +1,19 @@
+# Aula 04 - Type
+""" 
+    Então, já vimos o que são variáveis, os tipos de dados numericos e os tipos de dados de textos e agora vemos ver um método para verificar qual o tipo de dado eu estou usando.
+    Para isso basta eu usar a função  type().
+    Ex.:
+"""
+a = "Ola Mundo"
+b = 5
+c = 3.5
+d = 1+2j
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+""" 
+    Dessa forma conseguimos saber qual tipo de dado está sendo armazenada na variável.
+"""
