789d08842cb4f90892f54dce2d8f7db878d62d75
valdean132 <65917804+valdean132@users.noreply.github.com>
2022-10-08 20:54:28 -0400

    Python - Escopo de variáveis
    Nessa aula vimos um pouco sobre o escopo de variáveis.

---
+++ Curso de Python Completo/Módulos/Módulo #3 -  Variáveis e Tipos de Dados/Aula 07/Escopo-de-Variáveis.py
@@ -0,0 +1,12 @@
+""" Nessa aula falaremos sobre o Escopo de variáveis.
+
+    O escopo de variáveis é o limite, o objetivo e o proposito de alguma coisa. No nosso caso é o limite, objetivo e o proposito das variaveis.
+
+    Podemos pensar nelas, para começar, como limite.
+    
+    Existem dois tipos principais de variáveis, as globais e as locais.
+    Suas diferenças são:
+        Globais -> São declaradas fora de todos os blocos e funções e são acessíveis em qualquer parte do código, também pode ser usadas e modificadas por todas as outras funções e existirão durante toda a execução do programa;
+        
+        Locais -> São declaradas dentro do bloco de uma função. Não podem ser usadas ou modificadas por outras funções e elas somente existe dentro da quela função na qual foi criada e enquanto essa tal função estiver sendo executada.
+"""
