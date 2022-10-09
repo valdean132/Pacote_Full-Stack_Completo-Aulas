c746df791a58764b1ba6dcf95c45944ef8c6249c
valdean132 <65917804+valdean132@users.noreply.github.com>
2022-10-08 21:03:36 -0400

    Python - Tipos de Dados
    Nessa aula vimos um resumo desse modulo

---
+++ Curso de Python Completo/Módulos/Módulo #3 -  Variáveis e Tipos de Dados/Aula 08/tipos-de-Dados.py
@@ -0,0 +1,20 @@
+""" 
+    Variáveis e os Tipos de Dados
+    
+    Tipo de Dados:
+    
+    Tipos de Texto: str
+    Tipos de Númericos: int, float, complex
+    
+    Tipos de Sequência: list, tuple, range
+    Tipos de Mapeamento: dict
+    Tipos Set: set, frozenset
+    
+    Tipo Booleano: bool
+    Tipos binários: bytes, bytearray, memoryview
+    
+    print()
+    type()
+    #casting float, int, str 
+
+"""
