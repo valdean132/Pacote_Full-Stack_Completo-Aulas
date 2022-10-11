076c9d084ec30171507e70e64992e64ba5c6cda5
valdean132 <65917804+valdean132@users.noreply.github.com>
2022-10-11 06:10:13 -0400

    Python - Operadores Aritméticos
    Nessa aula vimos um pouco sobre Operadores Aritméticos e como eles funcionam.

---
+++ Curso de Python Completo/Módulos/Módulo #4 - Estruturas Lógicas e Condicionais/Aula 03/operadores-aritméticos.py
@@ -0,0 +1,34 @@
+"""
+
+    Existem diversos tipos de opreadores. Mas, hoje aprederemos sobre os Operadores Aritméticos
+    
+    Para começar temos o operador de atribuição que é definido pelo sinal de igualdade "=", ou seja, toda vez que ele aparecer no código quer dizer que ele está atribuindo algo a alguma variável. Por exemplo:
+        x = 5
+       
+    Agora vamos aos operadores aritiméticos que são eles:
+        Operador da soma/adção => "+"
+        Operador da subtração => "-"
+        Operador da multiplicação => "*"
+        Operador da divisão => "/"
+    
+    Para testar vamos fazer alguns exemplo a baixo para ser xecutados e ver o que sai.
+"""
+x = 3
+y = 5
+
+# Soma/Adção
+z = x + y
+
+# Subtração
+z = y - x
+
+# Multiplicação
+z = x * y
+
+# Divisão
+z = y / x
+
+# Saída
+print(z)
+
+#  Vale lembrar que no caso da divisão ele sempre deixara seu resultado em float já que pode nã haver números inteiros nele
