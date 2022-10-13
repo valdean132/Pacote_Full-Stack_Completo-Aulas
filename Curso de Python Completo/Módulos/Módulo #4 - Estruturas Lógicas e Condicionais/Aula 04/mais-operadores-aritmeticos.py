e44cdc6bd476e10c6e1686f5db30efeff106162d
valdean132 <65917804+valdean132@users.noreply.github.com>
2022-10-13 05:52:47 -0400

    Python - Mais Operadores Aritméticos
    Nessa aula vimos um pouco mais sobre Operadores Aritméticos

---
+++ Curso de Python Completo/Módulos/Módulo #4 - Estruturas Lógicas e Condicionais/Aula 04/mais-operadores-aritmeticos.py
@@ -0,0 +1,12 @@
+"""
+    Nessa Aula vamos aprender mais sobre outros Operadores Aritméticos.
+
+    Na AUla passada aprendemos os principais operadores, hoje aprenderemos outros tão importantes quanto. Vamos lá
+
+    São eles:
+        Operador de modulo/resto => "%"
+        Operador da exponenciação => "**"
+        Operador da divisão inteira => "//"
+
+    No modulo ou resto, ele me retorna tudo o que sobra de uma divisão, muito útil para saber se um número é impa ou par.
+"""
