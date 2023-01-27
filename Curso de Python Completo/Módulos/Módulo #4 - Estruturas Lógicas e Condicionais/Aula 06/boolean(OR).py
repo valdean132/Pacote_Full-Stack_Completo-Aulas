964d5ed4cf3660aedb71edcb9eb2eecbfeeae602
valdean132 <65917804+valdean132@users.noreply.github.com>
2023-01-26 21:17:38 -0400

    Python - boolean(OR)
    Vendo como funciona a booleana OR e NOT

---
+++ Curso de Python Completo/Módulos/Módulo #4 - Estruturas Lógicas e Condicionais/Aula 06/boolean(OR).py
@@ -0,0 +1,30 @@
+""" 
+    Operadores lógicos e o Tipo Boolean
+    
+    Em ciência da computação, boolean, ou logico, é um tipo de dado primitivo que possui dois valores, que podem ser considerados como (0 ou 1), falso ou verdadeiro.
+    
+    Chamado boolean em homenagem a George Boole
+    
+"""
+
+# Tipo Boolean: True e False - Ambas as palavras devem ser escritas com letras maiusculas.
+
+""" 
+    Operadpres Lógicos:
+        and (e)
+        or (ou)
+        not (negação)
+
+    Tabela Verdade:
+        Utilizando o "E" (and):
+            Verdadeiro e Verdadeiro - True and True = True
+            Verdadeiro e Falso - True and False = False
+            Falso e Verdadeiro - False and True = False
+            Falso e False - False and False = False
+        
+        Utilizando o "OU" (or):
+            Verdadeiro ou Verdadeiro - True or True = True
+            Verdadeiro ou Falso - True or False = True
+            Falso ou Verdadeiro - False or True = True
+            Falso ou False - False or False = False
+"""
