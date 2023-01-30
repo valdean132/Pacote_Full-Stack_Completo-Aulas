16b0691d8d1c9923ecb84ce4a3c4113bfbedb281
valdean132 <65917804+valdean132@users.noreply.github.com>
2023-01-29 22:53:44 -0400

    Python - Operadores de associação
    Nessa aula vimos os dois exemplos de associação que temos com o Python.

---
+++ Curso de Python Completo/Módulos/Módulo #4 - Estruturas Lógicas e Condicionais/Aula 09/Operadores de Associação.py
@@ -0,0 +1,20 @@
+""" 
+    Operadores de associação: Comparam se algum elemento o objeto está contido em outro
+
+    Ex.: 
+        nome = "marcos"
+    
+    No exemplo acima, a variavel está lendo um caracter por vez, pois isso faz mais sentido lógico.
+    
+    Operadores de associação:
+        Operador "in": x in y
+        Operador "not in": x not in y
+
+"""
+
+x = 'pernambuco'
+
+y = "per"
+
+print(y not in x)
+print(y in x)
