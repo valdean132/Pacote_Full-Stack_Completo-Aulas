8411f388a0761e79b1eb4d8f498efa3c220559a6
valdean132 <65917804+valdean132@users.noreply.github.com>
2023-01-31 06:56:58 -0400

    Python - Exercício #2
    Finalizando o segundo exercício passado em Python.

---
+++ Curso de Python Completo/Módulos/Módulo #4 - Estruturas Lógicas e Condicionais/Aula 13/Exercício #2.py
@@ -0,0 +1,39 @@
+""" 
+    Exercício 02.
+"""
+
+#? 01 - Verificar se usuário é MENOR, EMANCIPADO ou MAIOR de idade ?#
+# print("======================***======================\n")
+# print("Informe sua idade e veremos se Você é MENOR, EMANCIPADO ou MAIOR de idade:")
+# print("=***==***=\n")
+# idade = int(input("Qual sua idade:\n  Idade: "))
+# print("\n=***==***=\n")
+# if idade < 16:
+#     print("Parece que você ainda é menor de idade...")
+# elif idade >= 16 and idade < 18:
+#     print("Parece que você ainda é emancipado...")
+# else:
+#     print("Parece que você já é maior de idade...")
+# print("\n=***==***=")
+# print("\n======================***======================")
+
+#? 02 - receba a idade de um nadador e imprima a sua categoria seguindo as regras ?#
+# print("======================***======================\n")
+# print("Informe sua idade e veremos de qual categoria você pertence:")
+# print("=***==***=\n")
+# idade = int(input("Qual sua idade:\n  Idade: "))
+# print("\n=***==***=\n")
+# if idade < 5:
+#     print("Parece que você ainda não pode pertencer a uma Categoria...")
+# elif idade >= 5 and idade <= 7:
+#     print("Você está na categoria Infantil A")
+# elif idade >= 8 and idade <= 10:
+#     print("Você está na categoria Infantil B")
+# elif idade >= 11 and idade <= 13:
+#     print("Você está na categoria Juvenil A")
+# elif idade >= 14 and idade <= 17:
+#     print("Você está na categoria Juvenil B")
+# else:
+#     print("Parece que você não tem mais idade para participar...")
+# print("\n=***==***=")
+# print("\n======================***======================")
