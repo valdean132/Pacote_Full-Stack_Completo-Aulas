2bb9a730365bf28331343b8f95a88be04f84ca79
valdean132 <65917804+valdean132@users.noreply.github.com>
2023-02-14 07:16:34 -0400

    Python - For e Range
    Nessa aula vimos o funcionamento do For e com ele também vimos como utilizar o Range juntamente com o for

---
+++ Curso de Python Completo/Módulos/Módulo #5 - Estruturas de Repetição/Aula 03/for e range.py
@@ -0,0 +1,30 @@
+""" 
+    Estrutura de Repetição
+    
+    For:
+
+"""
+
+""" 
+s = 'Manaus'
+
+for x in s:
+    print(x)
+"""
+""" 
+for x in range(6):  # Range(6) significa que começamos do 0 até termos 6 digitos
+    print(x)
+"""
+""" 
+for x in range(5,5): # Incrementa  apartir do primeiro parametro
+    print(x)
+"""
+""" 
+for x in range(0, 6, 1):  # Incrementa a partir do primeiro parametro, vai até o segundo parametro e incrementa conforme o terceiro parametro
+    print(x)
+"""
+
+for x in range(6):
+    print(x)
+else:  # Caso o For acabe ele entra nessa condição 
+    print('Chegamso ao fim')
