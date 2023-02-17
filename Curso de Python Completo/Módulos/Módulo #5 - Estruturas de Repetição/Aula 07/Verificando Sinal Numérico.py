bab95170a7cf1ac1a9d1802ee606d6d9acae626a
valdean132 <65917804+valdean132@users.noreply.github.com>
2023-02-16 21:39:20 -0400

    Python - Verificando Sinal Numérico
    Nessa aula vimos como verificar se um numero é positivo, igual a 0 ou negativo usando o if e elif

---
+++ Curso de Python Completo/Módulos/Módulo #5 - Estruturas de Repetição/Aula 07/Verificando Sinal Numérico.py
@@ -0,0 +1,9 @@
+
+numero = int(input("Digite um numero: "))
+
+if numero > 0:
+    print(f"O {numero} é positivo")
+elif numero == 0:
+    print(f"O {numero} é igual a 0")
+else:
+    print(f"O {numero} é negativo")
