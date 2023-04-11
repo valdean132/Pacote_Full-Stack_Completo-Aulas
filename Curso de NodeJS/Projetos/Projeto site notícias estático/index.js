af5fa574eb282458b9fa3251a204f0051c229fa2
valdean132 <65917804+valdean132@users.noreply.github.com>
2023-04-10 22:03:49 -0400

    NodeJS - Desenvolvendo portal 1/3
    Nessa aula iniciei o meu primeiro projeto em NodeJS sendo ele um portal de noticias, Estamos no front-end.

---
+++ Curso de NodeJS/Projetos/Projeto site notícias estático/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/pages'));
+
+app.get('/', (req, res) => {
+
+    if(req.query.busca != null){
+        res.send('Você Buscor por: '+req.query.busca)
+    }else{
+        res.render('home', {});
+    }
+
+});
+
+app.get('/:slug', (req, res) => {
+    res.send(req.params.slug);
+});
+
+
+app.listen(5000, () => {
+    console.log('Servidor rodando');
+})
