f817dd14050957644bcef87e45d3f032f854d264
valdean132 <65917804+valdean132@users.noreply.github.com>
2023-04-13 08:51:27 -0400

    NodeJs - Sistema de chat com socket
    Nesse modulo estamos criando um sistema de chat e vendo um pouco melhor sobre o Socket.io.

---
+++ Curso de NodeJS/Projetos/sistema de chat/index.js
@@ -0,0 +1,20 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+var usuarios = [];
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection',(socket)=>{
+    socket.on('chat message', obj => {
+        io.emit('chat message', obj);
+    });
+});
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
